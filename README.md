e23a136f28bc6ffa017a01b5fa3a86ca03b10070
Scott <chasing6@users.noreply.github.com>
2019-03-05 16:41:34 -0800

    Rename plugin

---
+++ README.md
@@ -0,0 +1,12 @@
+# Plugin Name 
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
+
+Adds a Birdeye Feed
+
+
+## Description 
+Adds a Birdeye Feed
+
+
+## Changelog 
--- birdeye-wp.php
+++ birdeye-wp.php
@@ -1,24 +1,18 @@
 <?php
 
 /**
- * The plugin bootstrap file
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://caseydallen.com
+ * @link              https://github.com/chasing6/
  * @since             1.0.0
  * @package           Birdeye_Wp
  *
  * @wordpress-plugin
- * Plugin Name:       Birdeye WP
- * Plugin URI:        http://caseydallen.com
+ * Plugin Name:       C6 Birdeye WP
+ * Plugin URI:        https://github.com/chasing6/
  * Description:       Birdeye reviews api
- * Version:           0.1.1
- * Author:            Casey Allen
- * Author URI:        http://caseydallen.com
+ * Version:           0.1.2
+ * Author:            Scott McCoy
+ * Author URI:        https://github.com/chasing6/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       birdeye-wp
@@ -74,3 +68,5 @@ function run_birdeye_wp() {
 
 }
 run_birdeye_wp();
+
+//do_action('wp_rtm/add', plugin_dir_path( __FILE__ ) );
